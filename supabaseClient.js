a2d240f3f52787b8137c77129540665d0e33505b
roger13129-ops <roger13129@gmail.com>
2026-07-19 19:09:10 +0800

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,9 +1,1 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-// 請在此處貼上您的 Supabase URL (Supabase URL)
-const SUPABASE_URL = "https://iwiceompztrcggnceoon.supabase.co";
-
-// 請在此處貼上您的 Supabase API Key (anon public key)
-const SUPABASE_PUBLIC_KEY = "sb_publishable_DWCH7VfKxYOyixzZXnjnVQ_0wnjrRBR";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
